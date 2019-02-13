62ff5028d11cfefb3287aea060d3b1543ac1391d
Richelle Tan <r_kyj89@naver.com>
2019-02-13 14:08:25 +0800

    Update resize.py

---
+++ Python/resize.py
@@ -0,0 +1,15 @@
+from PIL import Image
+from sys import argv
+
+if len(argv) != 4:
+    exit("usage: python resize.py n infile outfile")
+
+n = int(argv[1])
+infile = argv[2]
+outfile = argv[3]
+
+inimage = Image.open(infile)
+width, heigh = inimage.size
+outimage = inimage.resize((width * n, height * n))
+
+outimage.save(outfile)
