3da96d7bb692c7795cde7d4c53ff95b20a3814e9
Richelle Tan <r_kyj89@naver.com>
2019-02-22 18:24:31 +0800

    add cough.py files

---
+++ Harvard's CS50/cough0.py
@@ -0,0 +1,8 @@
+def cough():  # defining an object
+    print("cough")  # will run this if found on the botton
+# define first as python reads code from up to down
+
+
+# loop
+for i in range(3):
+    cough()
