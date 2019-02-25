ef6e21f179eee88f923488f53a74e9f87f6c6a1c
Richelle Tan <r_kyj89@naver.com>
2019-02-25 23:40:30 +0800

    added new txt.file + added code to regressor.py

--- Python - Real World Machine Learning/regressor.py
+++ Python - Real World Machine Learning/regressor.py
@@ -2,6 +2,7 @@ import sys
 import numpy as np
 from sklearn import linear_model
 import matplotlib.pyplot as plt
+import _pickle as pickle
 
 filename = sys.argv[1]
 x = []
@@ -49,3 +50,31 @@ plt.scatter(x_test, y_test, color='green')
 plt.plot(x_test, y_test_pred, color='black', linewidth=4)
 plt.title('Test Data')
 plt.show()
+
+# new / regressor accuracy
+print("Mean absolute error=", round(sm.mean_absolute_error(y_test, y_test_pred), 2))
+print("Mean squared error=", round(sm.mean_squared_error(y_test, y_test_pred), 2))
+print("Median absolute error=", round(sm.median_absolute_error(y_test, y_test_pred), 2))
+print("Explained variance score=", round(sm.explained_variance_score(y_test, y_test_pred), 2))
+print("R2 score=", round(sm.r2_score(y_test, y_test_pred), 2))
+
+
+output_model_file = 'saved_model.pkl'
+with open(output_model_file, 'w') as f:
+    pickle.dump(linear_regressor, f)
+with open(output_model_file, 'r') as f:
+    model_linregr = pickle.load(f)
+
+y_test_pred_new = model_linregr.predict(x_test)
+print("\nNew mean absolute error=", round(sm.mean_absolute_error(y_test, y_test_pred_new), 2))
+
+ridge_regressor = linear_model.Ridge(alpha=0.01, fit_intercept=True, max_iter=10000)
+# the alpha parameter controls the complexity
+# As alpha gets closer to 0, the ridge regressor tends to become more like a linear regressor with ordinary least squares. So, if you want to make it robust against outliers, you need to as- sign a higher value to alpha. We considered a value of 0.01, which is moderate.
+ridge_regressor.fit(x_train, y_train)
+y_test_pred_ridge = ridge_regressor.predict(x_test)
+print("Mean absolute error=", round(sm.mean_absolute_error(y_test, y_test_pred_ridge), 2))
+print("Mean squared error=", round(sm.mean_squared_error(y_test, y_test_pred_ridge), 2))
+print("Median absolute error=", round(sm.median_absolute_error(y_test, y_test_pred_ridge), 2))
+print("Explained variance score=", round(sm.explained_variance_score(y_test, y_test_pred_ridge), 2))
+print("R2 score=", round(sm.r2_score(y_test, y_test_pred_ridge), 2))
