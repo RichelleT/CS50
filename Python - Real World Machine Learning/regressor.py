ce12353f09061bb930e46fbe686f035f73beeb0b
Richelle Tan <r_kyj89@naver.com>
2019-02-26 00:20:15 +0800

    added regacc.py

--- Python - Real World Machine Learning/regressor.py
+++ Python - Real World Machine Learning/regressor.py
@@ -52,12 +52,6 @@ plt.title('Test Data')
 plt.show()
 
 # new / regressor accuracy
-print("Mean absolute error=", round(sm.mean_absolute_error(y_test, y_test_pred), 2))
-print("Mean squared error=", round(sm.mean_squared_error(y_test, y_test_pred), 2))
-print("Median absolute error=", round(sm.median_absolute_error(y_test, y_test_pred), 2))
-print("Explained variance score=", round(sm.explained_variance_score(y_test, y_test_pred), 2))
-print("R2 score=", round(sm.r2_score(y_test, y_test_pred), 2))
-
 
 output_model_file = 'saved_model.pkl'
 with open(output_model_file, 'w') as f:
