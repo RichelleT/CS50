d4a50d4dca738ea409e8294a093ae2f61f0c3f8d
Richelle Tan <r_kyj89@naver.com>
2019-03-04 22:44:08 +0800

    Update preprocessor.py

--- Artificial Intelligence with Python/preprocessor.py
+++ Artificial Intelligence with Python/preprocessor.py
@@ -7,3 +7,18 @@ input_data = np.array([[5.1, -2.0, 3.3], [-1.2, 7.8, -6.1], [3.9, -9.9, 2.1], [7
 # binarizing data
 data_binarized = preprocessing.Binarizer(threshold=2.1).transform(input_data)
 print("\nBinarized Data:\n", data_binarized)
+
+# print function for mean
+print("\nBEFORE:")
+print("Mean =", input_data.mean(axis=0))
+
+# print function for standard deviation
+print("Standard Deviation = ", input_data.std(axis=0))
+
+# remove mean
+# defining data_scaler
+data_scaled = preprocessing.scale(input_data)
+
+print("\nAFTER:")
+print("Mean =", data_scaled.mean(axis=0))
+print("Standard Deviation =", data_scaled.std(axis=0))
