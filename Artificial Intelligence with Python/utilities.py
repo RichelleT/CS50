75d8f72ff72b712436188835d5c54cf8033e296d
Richelle Tan <r_kyj89@naver.com>
2019-03-07 17:53:27 +0800

    Update utilities.py

--- Artificial Intelligence with Python/utilities.py
+++ Artificial Intelligence with Python/utilities.py
@@ -2,3 +2,7 @@ import numpy as np
 from sklearn import linear_model
 import matplotlib.pyplot as plt
 from utilities import visualize_classifier
+
+# defining the sample data under variable x and y for two dimensional vector
+x =
+y =
