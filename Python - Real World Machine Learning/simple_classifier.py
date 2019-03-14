1b0cf3bc430e97b9d2346f9520e4dbaeb95d6ece
Richelle Tan <r_kyj89@naver.com>
2019-03-14 16:36:29 +0800

    Update simple_classifier.py

--- Python - Real World Machine Learning/simple_classifier.py
+++ Python - Real World Machine Learning/simple_classifier.py
@@ -2,4 +2,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 X = np.array([[3,1], [2,5], [1,8], [6,4], [5,2], [3,5], [4,7], [4,-1]])
-y = [0, 1, 1, 0, 0, 1, 1, 0] 
+y = [0, 1, 1, 0, 0, 1, 1, 0]
+
+class_0 = np.array([X[i] for i in range(len(X)) if y[i]==0])
+class_1 = np.array([X[i] for i in range(len(X)) if y[i]==1])
+
+plt.figure()
+plt.scatter(class_0[:,0], class_0[:,1], color='black', marker='s')
+plt.scatter(class_1[:,0], class_1[:,1], color='black', marker='x')
+
+# this is a scatter plot, where we use squares and crosses to plot points.
+# the marker parameter specifies the shape you want to use
+# square is used to denote the points in class_0
+# crosses is used to denote the point in class_1
