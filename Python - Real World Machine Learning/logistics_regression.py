b4d87c8ccc31f82858f2936ccc9f39c5ef6871fa
Richelle Tan <r_kyj89@naver.com>
2019-03-15 15:15:43 +0800

    Update logistics_regression.py

--- Python - Real World Machine Learning/logistics_regression.py
+++ Python - Real World Machine Learning/logistics_regression.py
@@ -13,5 +13,26 @@ classifier.fit(X, y)
 plot_classifier(classifier, X, y)
 
 def plot_classifier(classifier, X, y):
-    #defines the range to plot the figure
+    # defines the range to plot the figure
     x_min, x_max = min(X[:,0]) -1.0, max(X[",0"]) + 1.0
+    y_min, x_min = min(X[:,1]) - 1.0, max(X[;,1]) + 1.0
+
+    # this denotes the step size that will be used for the mesh grid
+    step_size = 0.01
+
+    # this defines the mesh grid
+    x_values, y_values = np.meshgrid(np.arange(x_min, x_max, step_size), np.arange(y_min, y_max, step_size))
+    # x_values and y_values variable contains the grid of points where the function will be evaluated
+
+    # this will compute the classifier output
+    mesh_output = classifier.predict(np.c_[x_values.ravel(), y_values.ravel()])
+
+    # this will reshape the array
+    mesh_output = mesh_output.reshape(x_values.shape)
+
+    #this will plot the output using a colored plot_classifier
+    plt.figure()
+
+    # this is the color scheme for the plot
+    plt.pcolormesh(x_values, y_values, mesh_output, cmap=plt.cm.gray)
+    
