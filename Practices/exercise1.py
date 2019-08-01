b159ba942af32be7e75c9d5ad7a80b5ce0c70be2
Richelle Tan <r_kyj89@naver.com>
2019-08-01 20:44:25 +0800

    Update exercise1.py

--- Practices/exercise1.py
+++ Practices/exercise1.py
@@ -1,11 +1,11 @@
+"""
+Create a program that asks the user to enter their name and their age.
+Print out a message addressed to them that tells them the year that they will turn 100 years old.
+"""
+
 userName = input ("Please enter your name: ")
 print ("Your name is " + userName)
 
 age = int (input ("What is your current age? "))
 age2 = 100 - age
 print ("The amount of year(s) that " + userName + " will turn 100 years old is " + str(age2))
-
-"""
-Create a program that asks the user to enter their name and their age.
-Print out a message addressed to them that tells them the year that they will turn 100 years old.
-"""
