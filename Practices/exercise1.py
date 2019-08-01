7a190973b7ca9e3bd14bfae1ba4ee49d73caa13d
Richelle Tan <r_kyj89@naver.com>
2019-08-01 20:40:25 +0800

    Update exercise1.py

---
+++ Practices/exercise1.py
@@ -0,0 +1,6 @@
+userName = input ("Please enter your name: ")
+print ("Your name is " + userName)
+
+age = int (input ("What is your current age? "))
+age2 = 100 - age
+print ("The amount of year(s) that " + userName + " will turn 100 years old is " + str(age2))
